10ce1d1fde7ed6a400cade3cc00ea89a8bf87d1c
Andres <a.barroso.2016.alumnos.urjc.es>
2018-09-29 13:27:54 +0200

    creo archivo calcoohija

---
+++ calcoohija.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+
+class Calculadora:
+
+	def suma(self, op1, op2):
+		return op1 + op2
+	def resta(self, op1, op2):
+		return op1 - op2
+
+calc = Calculadora()
+
+if __name__ == "__main__":
+	
+	
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+	
+    if sys.argv[2] == "suma":
+        result = calc.suma(operando1,operando2)
+    elif sys.argv[2] == "resta":
+        result = calc.resta(operando1,operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
+
+    print(result)
