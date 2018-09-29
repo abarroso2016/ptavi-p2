233beea5ca706202239891f2b37ca1c3ea969907
Andres <a.barroso.2016.alumnos.urjc.es>
2018-09-29 14:07:42 +0200

    Creo la clase CalculadoraHija sin excepcion en la division

--- calcoohija.py
+++ calcoohija.py
@@ -9,7 +9,14 @@ class Calculadora:
 	def resta(self, op1, op2):
 		return op1 - op2
 
-calc = Calculadora()
+class CalculadoraHija(Calculadora):
+	
+	def multiplicar(self, op1, op2):
+		return op1 * op2
+	def dividir(self, op1, op2):
+		return op1 / op2
+	
+calc = CalculadoraHija()
 
 if __name__ == "__main__":
 	
@@ -25,6 +32,10 @@ if __name__ == "__main__":
         result = calc.suma(operando1,operando2)
     elif sys.argv[2] == "resta":
         result = calc.resta(operando1,operando2)
+    elif sys.argv[2] == "multiplica":
+        result = calc.multiplicar(operando1,operando2)
+    elif sys.argv[2] == "divide":
+        result = calc.dividir(operando1,operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
