97f8887b3ddbc8d1a37d51c19a83415bb2df54be
Andres <a.barroso.2016@alumnos.urjc.es>
2018-10-01 01:20:14 +0200

    Creo el programa en el fichero creado anteriormente, buen funcionamiento

--- calcplus.py
+++ calcplus.py
@@ -1,1 +1,55 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import csv
 
+from calcoohija import CalculadoraHija
+
+
+calc = CalculadoraHija()
+
+if __name__ == "__main__":
+    with open('example.csv', newline='') as File:  
+        reader = csv.reader(File)
+        for row in reader:
+           if row[0] == "suma":
+               plus=0
+               j=0
+               for i in row:
+                   if j != 0:
+                       plus=calc.suma(plus,int(row[j]))
+                   j=j+1
+           elif row[0] == "resta":
+               minum=0
+               j=0
+               for i in row:
+                   if j != 0 and j != 1:
+                       minum=calc.resta(minum,int(row[j]))
+                   elif j == 1:
+                       minum=calc.suma(minum,int(row[1]))
+                   j=j+1
+           elif row[0] == "multiplica":
+               multiply=0
+               j=0
+               for i in row:
+                   if j != 0 and j != 1:
+                       multiply=calc.multiplicar(multiply,int(row[j]))
+                   elif j == 1:
+                       multiply=calc.suma(multiply,int(row[1]))
+                   j=j+1
+           elif row[0] == "divide":
+               div=0
+               j=0
+               for i in row:
+                   if j != 0 and j != 1:
+                       try:
+                           div=calc.dividir(div,int(row[j]))
+                       except ZeroDivisionError:
+                           sys.exit("Division by zero is not allowed")
+                   elif j == 1:
+                       div=calc.suma(div,int(row[1]))
+                   j=j+1
+print(plus)
+print(minum)
+print(multiply)
+print(div)
